07f1ecc04ca9e94a4f36a3fc5cbae88f4aa775c0
kit sitou <ctwohofa@gmail.com>
2026-04-29 19:12:33 -0700

    detail view

---
+++ BookManagerKit/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  BookManagerKit
+//
+//  Created by Kit Sitou on 4/28/26.
+//
+
+import Foundation
---
+++ BookManagerKit/BookDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailView.swift
+//  BookManagerKit
+//
+//  Created by Kit Sitou on 4/28/26.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookDetailView()
+}
