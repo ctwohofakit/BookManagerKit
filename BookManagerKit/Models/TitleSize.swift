18e5afa2601766a691fe93b30f438f00faff7986
kit sitou <ctwohofa@gmail.com>
2026-05-13 21:45:28 -0700

    2 settings added

---
+++ BookManagerKit/Models/TitleSize.swift
@@ -0,0 +1,12 @@
+//
+//  Untitled.swift
+//  BookManagerKit
+//
+//  Created by Kit Sitou on 5/13/26.
+//
+
+public enum titleSize: String, CaseIterable{
+    case small = "Small"
+    case medium = "Medium"
+    case big = "Big"
+}
